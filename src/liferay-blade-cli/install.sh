1914a315a130d6cce288e3af9802ab95cf55b167
Jamie Sammons <jamie.sammons@liferay.com>
2025-05-09 02:10:59 -0500

    Fix home path

--- src/liferay-blade-cli/install.sh
+++ src/liferay-blade-cli/install.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Installing Liferay Blade CLI"
 
-cd ${_REMOTE_USER_HOME} > /dev/null
+cd ${_REMOTE_USER_HOME} >/dev/null
 
 curl -L https://raw.githubusercontent.com/liferay/liferay-blade-cli/master/cli/installers/local | su - ${_REMOTE_USER} -c sh
+
